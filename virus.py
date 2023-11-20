10889c2d84375eed13205999627f2e80ef6ae2f7
el634dev <84924260+el634dev@users.noreply.github.com>
2023-11-20 17:51:35 -0500

    Completed virus.py

--- virus.py
+++ virus.py
@@ -1,15 +1,21 @@
 class Virus(object):
-    # Properties and attributes of the virus used in Simulation.
+    """
+    :desc: Properties and attributes of the virus used in Simulation.
+    :return: None
+    """
     def __init__(self, name, repro_rate, mortality_rate):
-        # Define the attributes of your your virus
+        """
+        :desc: Define the attributes of your virus
+        :return: None
+        """
         self.name = name
-        # TODO Define the other attributes of Virus
-        pass
-
+        # Define the other attributes of Virus
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
 # Test this class
 if __name__ == "__main__":
-    # Test your virus class by making an instance and confirming 
+    # Test your virus class by making an instance and confirming
     # it has the attributes you defined
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
